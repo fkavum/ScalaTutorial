86adcf0a899e67879ccc85d396165ad2541461b7
fkavum <38837509+fkavum@users.noreply.github.com>
2019-11-02 18:36:27 +0300

    statement and Expresssions

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -4,7 +4,7 @@ object HelloWorld {
 
   // kinda constant
   val name:String = "Pat Cook"
-  
+  val value = 42
   var age:Int = 22
   
   
@@ -21,12 +21,22 @@ object HelloWorld {
 
   }
   
-  
+  // This is an expression
   val message = name + " is " + (age+1) + " years old."
   // Yukarıdaki ile aynı sonucu verir.
   val messages2 = s"$name is ${age+1} years old."
   val str = s"The second element of t is ${t._2}."
   
   
+  value.+(age)
+  value + age
+  
+  value.min(age)
+  value min age +5
+  // same as because + operator has higher presidence
+  value min (age +5)
+  
+  // *5 is for repeating  it returns true btw if we exclude *5 
+  name == "Pat Cook"*5
   
 }
