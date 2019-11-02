df412aa80e8a0cfe09f6b211096cdc0a4ff1746b
fkavum <38837509+fkavum@users.noreply.github.com>
2019-11-02 20:43:08 +0300

    match expression

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -61,6 +61,18 @@ object HelloWorld {
        
        println(stuff)
       
+       
+       val fizzbuzz = for (i <- 1 to 20) yield {
+         (i%3 , i%5) match {
+           case(0,0) if i<25 => "fizzbuzz"
+              case(0,_) => "fizz"
+                 case(_,0) => "buzz"
+                    case _ => i.toString()
+         }
+       }
+       
+       
+       
       
   }
   
