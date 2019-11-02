ecd5581a153c1456e20a2078b7c24aaa7867267e
fkavum <38837509+fkavum@users.noreply.github.com>
2019-11-02 17:22:30 +0300

    Variables declared!

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,5 +1,23 @@
 
 
 object HelloWorld {
+
+  // kinda constant
+  val name:String = "Pat Cook"
   
+  var age:Int = 22
+  
+  
+  // String, Int, Double, Char, Boolean, Unit, Tupple
+  
+  val t =(1,2.7,"hi there")
+  val (a,b,c) = t
+  
+  // 0. indexi verir
+  println(t._1)
+  
+  def main(args: Array[String]):Unit = {
+     println("Hello World")
+
+  }
 }
