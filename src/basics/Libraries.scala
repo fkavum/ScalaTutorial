1e5135a4d1f45cae9fb5f86b1f76e64303368729
fkavum <38837509+fkavum@users.noreply.github.com>
2019-11-03 20:07:28 +0300

    text files

--- src/basics/Libraries.scala
+++ src/basics/Libraries.scala
@@ -2,11 +2,23 @@ package basics
 
 
 import scala.io.StdIn._
+import scala.io.Source
+import java.io.PrintWriter
 
 object Libraries {
   
   
   def main(args: Array[String]):Unit ={
+    
+    
+    val source = Source.fromFile("matrix.txt")
+    val lines = source.getLines()
+    val matrix = lines.map(_.split(" ").map(_.toDouble)).toArray
+    source.close()
+    
+    val pw = new PrintWriter("rowSums.txt")
+    matrix.foreach(row => pw.println(row.sum))
+    pw.close()
     
     println("What is your name?")
     val name = readLine()
