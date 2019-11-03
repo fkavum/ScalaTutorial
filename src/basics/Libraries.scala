a99c98a4b638a58eec6b8363c141b8fabbbd9837
fkavum <38837509+fkavum@users.noreply.github.com>
2019-11-03 15:47:41 +0300

    Array-List

--- src/basics/Libraries.scala
+++ src/basics/Libraries.scala
@@ -18,7 +18,23 @@ object Libraries {
     
   }
   
-  
+  /*
+   * Arrays - List - Tabulate - Fill
+   * val asd = Array(1,2,3)
+   * val asd = List (1,2,3)
+   * 
+   * 'b' :: asd
+   * 
+   * //100 tane elemanı 0 ile doldur.
+   * Array.fill(100)(0)
+   *  Array.fill(100)(math.random)
+   *  List.fill(5)(io.StdIn.readLine)
+   *  
+   *  Array.tabulate(10)(i=>i*i)
+   *  new Array[Int](20)
+   *  
+   *  
+   */
   
   
 }
