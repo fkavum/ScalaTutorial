08e6597b2b252551b47c09f8c0880bc541c84930
fkavum <38837509+fkavum@users.noreply.github.com>
2019-11-03 16:00:37 +0300

    basic sequence methods

--- src/basics/Libraries.scala
+++ src/basics/Libraries.scala
@@ -33,7 +33,41 @@ object Libraries {
    *  Array.tabulate(10)(i=>i*i)
    *  new Array[Int](20)
    *  
+   *  1 to 10 by 2
    *  
+   *  
+   *  a.drop(2)
+   *  a.dropRight(2)
+   *  a.head
+   *  a.tail
+   *  a.last
+   *  a.length
+   *  // iki array verir ilki 3 uzunluğunda olcak şekilde
+   *  a.splitAt(3)
+   *  --- İlk 3 yada son 3 elemanı list olarak verir
+   *  a.take(3)
+   *  a.takeRight(3)
+   *  // 2. idex dahil 5. index hariç yeni array
+   *  a.slice(2,5)
+   *  
+   *  3. index dahil 3 elemanı yok eder arayada 98,99 yerleştirir
+   *  a.patch(3,Array(98,99),3)
+   *  
+   *  // A nın içinde diff teki olmayan elemanları verir.
+   *  a.diff(Array(1,2,3,4))
+   *  a.intersect (array(1,2,3)) -> difin tam tersi
+   *  a.distinct -> unique değerler kalır bitek.
+   *  
+   *  
+   *  a.union
+   *  a.max
+   *  a.product
+   *  a.sorted
+   *  a.mkString
+   *  a.mkString(", ")
+   *  
+   *  a.zip()
+   *  a.zipWithIndex
    */
   
   
